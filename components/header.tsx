eeebd2d839f2e619bebf8a2cd52f255753180115
Bharath Sriraam R R <albharath.1305@gmail.com>
2023-11-25 16:24:52 -0500

    add header

---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { SpeakerIcon, User2Icon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="px-8 py-4 flex items-center justify-between w-full border-b border-2">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Scribee
+      </h3>
+
+      <div className="flex items-center space-x-4">
+        <Button variant="ghost" size="icon">
+          <SpeakerIcon />
+        </Button>
+
+        <Button variant="ghost" size="icon">
+          <User2Icon />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
