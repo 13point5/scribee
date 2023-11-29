ffa15c653bb16fd4419e9c2c8e6d40cabb87c153
Bharath Sriraam R R <albharath.1305@gmail.com>
2023-11-29 10:28:55 -0500

    gen story titles and images

---
+++ app/api/storyIdeas/route.ts
@@ -0,0 +1,44 @@
+import OpenAI from "openai";
+
+// Set the runtime to edge for best performance
+export const runtime = "edge";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  const { text } = await req.json();
+
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.chat.completions.create({
+    // model: "gpt-4-vision-preview",
+    model: "gpt-3.5-turbo",
+    stream: false,
+    messages: [
+      {
+        role: "user",
+        content: `Generate 3 short story titles for picture books that will be written by kids in grades 2-4 based on the following interest: ${text}. Give output as 3 titles separated in 3 lines without numbers for representing the order and dont add any extra messages. Keep the story titles short, creative, and easy to understand for kids in grades 2-4.`,
+      },
+    ],
+  });
+
+  const stories = response.choices[0].message.content?.split("\n") || [];
+
+  if (stories.length !== 3)
+    throw new Error("Could not generate 3 story titles");
+
+  const images = await Promise.all(
+    stories.map((storyTitle) =>
+      openai.images.generate({
+        model: "dall-e-3",
+        prompt: `Cover for a picture book titled: ${storyTitle}. This book is written by kids in grades 2-4 so adjust the style accordingly`,
+        n: 1,
+        size: "1024x1024",
+      })
+    )
+  );
+  const imgUrls = images.map((img) => img.data[0].url);
+
+  return Response.json({ stories, images: imgUrls });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import Header from "@/components/header";
 import {
   Canvas,
@@ -12,12 +12,13 @@ import {
 } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, Loader2Icon } from "lucide-react";
 import { exportAs } from "@/lib/utils";
 
 import axios from "axios";
 import * as ByteScale from "@bytescale/sdk";
 import StoryPage from "./story-page";
+import Toolbar from "@/components/toolbar";
 
 // export default StoryPage;
 
@@ -26,7 +27,13 @@ const DefaultSizeStyle = StyleProp.defineEnum("tldraw:size", {
   values: ["s", "m", "l", "xl"],
 });
 
+type StoryStateItem = { title: string; imgUrl: string };
+
 const StoryIdeaPage = () => {
+  const [generatingIdeas, setGeneratingIdeas] = useState(false);
+  const [stories, setStories] = useState<StoryStateItem[] | null>(null);
+  console.log("stories", stories);
+
   const handleMount = useCallback((editor: Editor) => {
     editor.batch(() => {
       if (editor.isIn("select")) {
@@ -47,7 +54,7 @@ const StoryIdeaPage = () => {
     <main className="flex min-h-screen flex-col items-center relative">
       <Header />
 
-      <div className="w-[745px] h-full grow flex flex-col items-center py-10 px-15 border-2">
+      <div className="w-[745px] h-full grow flex flex-col items-center py-10 px-15">
         <div className="relative mb-20">
           <img
             src="scribee1.svg"
@@ -66,8 +73,14 @@ const StoryIdeaPage = () => {
             <Canvas />
 
             <div className="w-full flex gap-4 justify-end z-[300] fixed top-[520px] right-[225px]">
-              <NextButton />
+              <NextButton
+                loading={generatingIdeas}
+                setLoading={setGeneratingIdeas}
+                setStories={setStories}
+              />
             </div>
+
+            <Toolbar />
           </Tldraw>
         </div>
       </div>
@@ -81,66 +94,79 @@ const uploadManager = new ByteScale.UploadManager({
   apiKey: "public_W142iLu7Bprevy3B942MBWvx28Gy", // This is your API key.
 });
 
-const NextButton = track(() => {
+const NextButton = track(({ loading, setLoading, setStories }) => {
   const editor = useEditor();
 
   const handleClick = async () => {
-    const img = await exportAs(editor);
-    if (!img) throw new Error("img is null");
-    // console.log("imgUrl", imgUrl);
+    setLoading(true);
+    setStories(null);
 
-    const res1 = await uploadManager.upload({ data: img });
-    // console.log("res", res1);
-    const { fileUrl } = res1;
-    console.log("fileUrl", fileUrl);
-    const resizedUrl = fileUrl.replace("/raw/", "/image/") + "?w=512&h=512";
-    console.log("resizedUrl", resizedUrl);
+    try {
+      const img = await exportAs(editor);
+      if (!img) throw new Error("img is null");
+      // console.log("imgUrl", imgUrl);
 
-    // return;
+      const res1 = await uploadManager.upload({ data: img });
+      // console.log("res", res1);
+      const { fileUrl } = res1;
+      console.log("fileUrl", fileUrl);
+      const resizedUrl = fileUrl.replace("/raw/", "/image/") + "?w=512&h=512";
+      console.log("resizedUrl", resizedUrl);
 
-    const res = await axios.post("/api/feedback", {
-      model: "gpt-4-vision-preview",
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              // text: "Give me feedback on mechanics for my story writing in the image attached",
-              text: "What's written in the image? Keep in mind its written by a kid in grades 2-4 and so the handwriting is very bad. Just return the text, no other message",
-            },
-            {
-              type: "image_url",
-              // image_url: resizedUrl,
-              image_url: {
-                url: resizedUrl,
-                detail: "low",
+      // return;
+
+      const res = await axios.post("/api/feedback", {
+        model: "gpt-4-vision-preview",
+        messages: [
+          {
+            role: "user",
+            content: [
+              {
+                type: "text",
+                // text: "Give me feedback on mechanics for my story writing in the image attached",
+                text: "What's written in the image? Keep in mind its written by a kid in grades 2-4 and so the handwriting is very bad. Just return the text, no other message",
               },
-            },
-          ],
-        },
-      ],
-    });
+              {
+                type: "image_url",
+                // image_url: resizedUrl,
+                image_url: {
+                  url: resizedUrl,
+                  detail: "low",
+                },
+              },
+            ],
+          },
+        ],
+      });
 
-    const text = res.data.response.choices[0].message.content;
-    console.log("text", text);
+      const text = res.data.response.choices[0].message.content;
+      console.log("text", text);
 
-    const storiesResponse = await axios.post("/api/feedback", {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `Generate 3 short story titles for picture books that will be written by kids in grades 2-4 based on the following interest: ${text}. Give output as 3 titles separated in 3 lines without numbers for representing the order and dont add any extra messages. Keep the story titles short, creative, and easy to understand for kids in grades 2-4.`,
-        },
-      ],
-    });
-    const stories = storiesResponse.data.response.choices[0].message.content;
-    console.log("stories", stories.split("\n"));
+      const res2 = await axios.post("/api/storyIdeas", {
+        text,
+      });
+      console.log("res2", res2);
+      setStories(
+        res2.data.stories.map((title: string, index: number) => ({
+          title,
+          imgUrl: res2.data.images[index],
+        }))
+      );
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Button onClick={handleClick}>
-      Next <ArrowRightIcon className="ml-2" />
+      Next{" "}
+      {loading ? (
+        <Loader2Icon className="animate-spin ml-2" />
+      ) : (
+        <ArrowRightIcon className="ml-2" />
+      )}
     </Button>
   );
 });
